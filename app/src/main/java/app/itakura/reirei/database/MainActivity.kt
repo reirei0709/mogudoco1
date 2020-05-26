549282e2bee42d5ab3073523e6aafe2be6789d88
mirei <usrmii301@gmail.com>
2020-05-26 22:20:40 +0900

    step1

---
+++ app/src/main/java/app/itakura/reirei/database/MainActivity.kt
@@ -0,0 +1,66 @@
+package app.itakura.reirei.database
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.annotation.IntegerRes
+import com.google.android.material.snackbar.Snackbar
+import io.realm.Realm
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Double.parseDouble
+
+class MainActivity : AppCompatActivity() {
+
+     val realm = Realm.getDefaultInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val memo:Memo? = read()
+
+        if (memo != null){
+            titleEditText.setText(memo.title)
+            idoeditText.setText(memo.ido)
+            keidoeditText.setText(memo.keido)
+
+        }
+
+        saveButton.setOnClickListener {
+            val title = titleEditText.text.toString()
+            val ido= idoeditText.text.toString().toDouble()
+            val keido = keidoeditText.text.toString().toDouble()
+            save(title,ido,keido)
+
+
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
+    }
+
+    fun read():Memo?{
+        return realm.where(Memo::class.java).findFirst()
+    }
+
+    fun save(title: String, content: String){
+        val memo: Memo? = read()
+
+        realm.executeTransaction {
+            if(memo != null){
+                memo.title = title
+                memo.ido= ido.toString().toDouble()
+                memo.keido = keido.toString().toDouble()
+            }else{
+                val newMemo: Memo = it.createObject(Memo::class.java)
+                newMemo.title = title
+                newMemo.ido = ido.toString().toDouble()
+                newMemo.keido = keido.toString().toDouble()
+            }
+
+            Snackbar.make(container,"登録出来ました！！", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+}
---
+++ app/src/main/java/app/itakura/reirei/database/RealmMemoApplication.kt
@@ -0,0 +1,25 @@
+package app.itakura.reirei.database
+
+import android.app.Application
+import android.os.Parcel
+import android.os.Parcelable
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+class RealmMemoApplication() : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Realm.init(this)
+        val realmConfig   = RealmConfiguration.Builder()
+            .deleteRealmIfMigrationNeeded()
+            .build()
+        Realm.setDefaultConfiguration(realmConfig)
+
+    }
+
+
+
+
+}
